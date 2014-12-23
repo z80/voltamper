8fc8f40df9c7457647cc4520d78c6387e8144641
unknown <z80@cortex-m3.(none)>
2014-12-23 10:32:53 -0800

    a

---
+++ firmware/voltamp/.dep/base64.o.d
@@ -0,0 +1,51 @@
+build/obj/base64.o: src/base64.c src/base64.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/inttypes.h \
+ c:\programs\yagarto\bin\../lib/gcc/arm-none-eabi/4.5.0/include/stdint.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/stdint.h \
+ c:\programs\yagarto\bin\../lib/gcc/arm-none-eabi/4.5.0/include/stddef.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/string.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/_ansi.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/newlib.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/sys/config.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/machine/ieeefp.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/sys/reent.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/_ansi.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/sys/_types.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/machine/_types.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/machine/_default_types.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/sys/lock.h \
+ c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/sys/string.h
+
+src/base64.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/inttypes.h:
+
+c:\programs\yagarto\bin\../lib/gcc/arm-none-eabi/4.5.0/include/stdint.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/stdint.h:
+
+c:\programs\yagarto\bin\../lib/gcc/arm-none-eabi/4.5.0/include/stddef.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/string.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/_ansi.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/newlib.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/sys/config.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/machine/ieeefp.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/sys/reent.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/_ansi.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/sys/_types.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/machine/_types.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/machine/_default_types.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/sys/lock.h:
+
+c:/programs/yagarto/lib/gcc/../../arm-none-eabi/sys-include/sys/string.h:
---
+++ firmware/voltamp/src/base64.h
@@ -0,0 +1,14 @@
+
+#ifndef BASE64_H_
+#define _BASE64_H_
+
+#include <inttypes.h>
+#include <string.h>
+
+int base64encode( const void* data_buf, size_t dataLength, char* result, size_t resultSize );
+int base64decode( char *in, size_t inLen, unsigned char *out, size_t * outLen );
+
+
+
+#endif
+
