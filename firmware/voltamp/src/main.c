ee74ae5325bcf3c3ae50d2405e6ca84e62b6ec46
unknown <z80@cortex-m3.(none)>
2015-06-11 15:39:37 -0700

    a

--- firmware/voltamp/src/main.c
+++ firmware/voltamp/src/main.c
@@ -14,18 +14,28 @@ int main(void)
     chSysInit();
 
     initLed();
-    //dacInit();
+    dacInit();
     //initAdc();
     //cpu_io_init();
 
     while (TRUE)
     {
+    	DacCfg dac;
     	//cpu_io_process();
         setLeds( 1 );
+    	dac.dac1 = 0;
+    	dac.dac2 = 0;
+        dacSet( &dac );
     	chThdSleepMilliseconds( 1000 );
         setLeds( 2 );
+    	dac.dac1 = 2047;
+    	dac.dac2 = 2047;
+        dacSet( &dac );
     	chThdSleepMilliseconds( 1000 );
         setLeds( 4 );
+    	dac.dac1 = 4095;
+    	dac.dac2 = 4095;
+        dacSet( &dac );
     	chThdSleepMilliseconds( 1000 );
     }
     return 0;
