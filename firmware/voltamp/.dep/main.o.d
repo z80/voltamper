2fb5f554f1016540babe084ee693eacb7c21c4cd
unknown <z80@cortex-m3.(none)>
2015-06-19 09:12:26 -0700

    a

--- firmware/voltamp/.dep/main.o.d
+++ firmware/voltamp/.dep/main.o.d
@@ -62,7 +62,8 @@ build/obj/main.o: src/main.c ../chibios_2.4.x/os/kernel/include/ch.h \
  ../chibios_2.4.x/os/hal/include/usb.h \
  ../chibios_2.4.x/os/hal/include/mmc_spi.h \
  ../chibios_2.4.x/os/hal/include/serial_usb.h src/iwdg/iwdg.h \
- src/iwdg/iwdg_lld.h src/led_ctrl.h src/dac.h src/adc_ctrl.h src/cpu_io.h
+ src/iwdg/iwdg_lld.h src/led_ctrl.h src/relay_ctrl.h src/dac.h \
+ src/adc_ctrl.h src/cpu_io.h
 
 ../chibios_2.4.x/os/kernel/include/ch.h:
 
@@ -205,6 +206,8 @@ src/iwdg/iwdg.h:
 src/iwdg/iwdg_lld.h:
 
 src/led_ctrl.h:
+
+src/relay_ctrl.h:
 
 src/dac.h:
 
