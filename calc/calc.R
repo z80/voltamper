b298c11aeb306e6688893fe7b14f15f0a81bb6ef
z80 <bashkirov.sergey@gmail.com>
2015-07-06 08:31:18 +0300

    a

--- calc/calc.R
+++ calc/calc.R
@@ -5,14 +5,14 @@ names( d )[2] <- "dac1"
 names( d )[3] <- "ref"
 names( d )[4] <- "aux"
 names( d )[5] <- "v"
-d$rr <- d$ref * d$ref
+d$rr <- as.numeric(d$ref) * as.numeric(d$ref)
 
 summary( d )
 #c = cov( d )
-model <- lm( v ~ ref + rr, data = d )
-summary( model )
+model <- lm( v ~ ref, data = d )
+summary( model )$sigma
 
-newd = data.frame( ref = d$ref ) 
+newd = data.frame( ref = d$ref )
 
 data <- predict( model, newd )
 #library( ridge )
@@ -21,3 +21,7 @@ plot( d$ref, d$v - data )
 coef( model )
 
 #linearRidge( formula, d )
+library( lasso2 )
+lm = l1ce( v ~ ref + rr, data = d, bound = 0.99999999 )
+summary(lm)$sigma
+summary(lm)$coef
